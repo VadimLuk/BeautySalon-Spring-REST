e037ecf08951d02adf69911a4ac4ceda147c5d6f
Vadym Lukianikhin <v.lukyanikhin@gmail.com>
2022-12-15 23:08:23 +0200

    Added GetMapping with Pagination, Filtering and Sorting

--- src/main/java/com/epam/beautysalonspring/controller/AppointmentController.java
+++ src/main/java/com/epam/beautysalonspring/controller/AppointmentController.java
@@ -4,13 +4,19 @@ import com.epam.beautysalonspring.dto.AppointmentDto;
 import com.epam.beautysalonspring.dto.groups.OnCreate;
 import com.epam.beautysalonspring.dto.groups.OnUpdate;
 import com.epam.beautysalonspring.exceptions.EntityNotFoundException;
+import com.epam.beautysalonspring.model.enums.AppointmentStatus;
 import com.epam.beautysalonspring.service.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
+
 
 @Slf4j
 @RestController
@@ -23,25 +29,40 @@ public class AppointmentController {
     @PostMapping
     public AppointmentDto bookAppointment(@RequestBody @Validated(OnCreate.class) AppointmentDto appointmentDto) {
         log.info("Layer: {}, Booking Appointment: {}", this.getClass().getSimpleName(), appointmentDto);
+
         return appointmentService.bookAppointment(appointmentDto);
     }
 
     @PatchMapping("/{appointmentId}")
     public AppointmentDto updateAppointment(@RequestBody @Validated(OnUpdate.class) AppointmentDto appointmentDto) {
         log.info("Layer: {}, Updating Appointment: {}", this.getClass().getSimpleName(), appointmentDto);
+
         return appointmentService.updateAppointment(appointmentDto);
     }
 
     @GetMapping("/{appointmentId}")
-    public AppointmentDto getAppointment(@PathVariable Long appointmentId) {
+    public AppointmentDto getAppointment(@PathVariable Long appointmentId) throws EntityNotFoundException {
         log.info("Layer: {}, Getting Appointment with Id: {}", this.getClass().getSimpleName(), appointmentId);
+
         return appointmentService.findAppointmentById(appointmentId);
     }
-    @GetMapping
-    public List<AppointmentDto> getAppointments(@PathVariable Long userId) throws EntityNotFoundException {
-        log.info("Layer: {}, Getting All Appointments for User with Id: {}", this.getClass().getSimpleName(), userId);
-        return appointmentService.findAppointmentsByUser(userId);
+
+    //appointments/filter?status=new&dateFrom=2022-12-14T00:00&dateTo=2022-12-14T23:59&sortBy=bookedDateTime&sortDir=asc
+    @GetMapping()
+    public Page<AppointmentDto> getAppointmentsPaginatedAndFiltered(@PathVariable Long userId,
+                                                                    @RequestParam AppointmentStatus status,
+                                                                    @RequestParam
+                                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                                    LocalDateTime dateFrom,
+                                                                    @RequestParam
+                                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                                    LocalDateTime dateTo,
+                                                                    @RequestParam(defaultValue = "0") Integer page,
+                                                                    @RequestParam(defaultValue = "10") Integer pageSize,
+                                                                    @RequestParam(required = false, defaultValue = "bookedDateTime") String sortBy,
+                                                                    @RequestParam(required = false, defaultValue = "asc") String sortDir) throws EntityNotFoundException {
+
+        PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.by(Sort.Direction.fromString(sortDir), sortBy));
+        return appointmentService.findAppointmentsByUserFilteredAndPaginated(userId, status, dateFrom, dateTo, pageRequest);
     }
-
-
 }
