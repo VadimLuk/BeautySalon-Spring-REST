aa2baeec497d2d20ca6db447fe3ff46a73bd143a
Vadym Lukianikhin <v.lukyanikhin@gmail.com>
2022-12-15 22:41:12 +0200

    Changed Validation Pattern For PhoneNumber.java, PhoneNumberValidator.java and null allowed

--- src/main/java/com/epam/beautysalonspring/validation/validators/PhoneNumberValidator.java
+++ src/main/java/com/epam/beautysalonspring/validation/validators/PhoneNumberValidator.java
@@ -8,18 +8,17 @@ import javax.validation.ConstraintValidatorContext;
 
 @Component
 public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
-    private String regexp;
     private String message;
 
     @Override
     public void initialize(PhoneNumber constraintAnnotation) {
-        this.regexp = constraintAnnotation.regexp();
         this.message = constraintAnnotation.message();
     }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         context.buildConstraintViolationWithTemplate(message);
-        return value.matches(regexp);
+        if (value == null) return true;
+        return value.matches("\\d{12}");
     }
 }
